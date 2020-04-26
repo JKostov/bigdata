288a4a04e83758b2fd6d60721ea1104fe1dc3bd7
JKostov <julijekos@gmail.com>
2020-04-26 18:27:21 +0200

    ML start

--- ml-batch/src/main/java/Main.java
+++ ml-batch/src/main/java/Main.java
@@ -1,10 +1,18 @@
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.functions;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.ml.linalg.VectorUDT;
+import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
-
-// TODO: number of accidents in city in the given month
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import scala.Tuple2;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,18 +37,64 @@ public class Main {
 
         Dataset<Row> dataSet = spark.read().option("header", "true").csv(csvFile);
 
-        ShowTrafficEventTypesCountByCityandTimeSpan(dataSet);
+//        ShowNumberOfAccidentsPerCity(dataSet);
+
+        Dataset<Row> test = dataSet.filter(dataSet.col("Type").equalTo("Accident").and(dataSet.col("City").equalTo("Los Angeles")))
+                .select(functions.unix_timestamp(functions.date_format(dataSet.col("StartTime(UTC)"), "yyyy-MM-01"), "yyyy-MM-dd").alias("Month"), dataSet.col("City"))
+                .groupBy("City", "Month")
+                .agg(functions.count("City").cast(DataTypes.DoubleType).alias("NumberOfAccidentsPerMonth"));
+
+        Dataset<Row> test2 = test.select(test.col("Month"), test.col("NumberOfAccidentsPerMonth"));
+
+        test2.show();
+
+        List<StructField> fields = new ArrayList<StructField>();
+        fields.add(DataTypes.createStructField("Month", DataTypes.DoubleType, true));
+        fields.add(DataTypes.createStructField("NumberOfAccidentsPerMonth", new VectorUDT(), true));
+        StructType schema = DataTypes.createStructType(fields);
+
+        JavaRDD<Tuple2<Double, Vector>> javaRddTuple = test2.rdd().toJavaRDD().map(f -> {
+            Object m = f.get(0);
+            long l = (long)m;
+
+            return new Tuple2<>((double) l, Vectors.dense((double)f.get(1)));
+        });
+
+        JavaRDD<Row> javaRddRow = javaRddTuple.map(tup -> {
+            Double doub = tup._1();
+            Vector vect = tup._2();
+            return RowFactory.create(doub, vect);
+        });
+
+        Dataset<Row> converted = spark.createDataFrame(javaRddRow, schema);
+
+        converted.show(10000);
+
+        LinearRegression lr = new LinearRegression()
+                .setMaxIter(10)
+                .setRegParam(0.3)
+                .setElasticNetParam(0.8);
+
+        LinearRegressionModel lrModel = lr.setLabelCol("Month").setFeaturesCol("NumberOfAccidentsPerMonth").fit(converted);
+
+        Date a = new Date(2017, Calendar.APRIL, 1);
+        double asd = a.getTime() / 1000.0;
+
+        System.out.println("PREDICTION---------------------------------------------------");
+        System.out.println(asd);
+        System.out.println(lrModel.predict(Vectors.dense(asd)));
+        System.out.println("PREDICTION---------------------------------------------------");
 
         spark.stop();
         spark.close();
     }
 
-    static void ShowTrafficEventTypesCountByCityandTimeSpan(Dataset<Row> ds) {
+    static void ShowNumberOfAccidentsPerCity(Dataset<Row> ds) {
 
         ds.filter(ds.col("Type").equalTo("Accident"))
-                .select(functions.unix_timestamp(functions.date_format(ds.col("StartTime(UTC)"), "yyyy-MM-00 00:00:00.0")).alias("Month"), ds.col("City"))
-//                .groupBy("City")
-//                .agg(functions.count("City"))
+                .select(functions.unix_timestamp(functions.date_format(ds.col("StartTime(UTC)"), "yyyy-MM-01"), "yyyy-MM-dd").alias("Month"), ds.col("City"))
+                .groupBy("City", "Month")
+                .agg(functions.count("City").alias("NumberOfAccidentsPerMonth"))
                 .show()
         ;
     }
